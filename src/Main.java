95e4b21235e906510c54c01f6b14af2a04c7b135
Nikita Fedisko <engarahlion@yandex.ru>
2024-10-21 16:33:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 111_111;
+        int value = 20;
+
+        int mils = amount / value;
+
+        System.out.println("Начислено " + mils + " миль");
+    }
+}
